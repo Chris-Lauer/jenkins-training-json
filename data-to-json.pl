13686e1267067575deb555031226501f8d983f82
Ubuntu <ubuntu@ip-172-26-7-113.us-west-2.compute.internal>
2017-08-01 16:40:42 +0000

    initial commit

---
+++ data-to-json.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+#sample-data
+use JSON;
+
+open(my $fh, "sample-data");
+@lines = <$fh>;
+close($fh);
+my @p1;
+#my @p1,@p5,@p10,@p30,@p50,@p100,@ep8,@e2,@e4;
+#print(@lines);
+my $i=0;
+foreach my $line (@lines){
+	if($line =~ /^(\d\d\d\d) (\d\d) (\d\d)\s+(\d+)\s+\d+\s+\d+\s+(.*)/){
+		#print "$1$2$3$4 $5\n";
+		my @fields = split(/\s+/,$5);
+	        $p1[$i] = ["$1$2$3$4",$fields[0]];
+		#print("test: $p1[$i][0] $p1[$i][1]\n"); 	
+		$i++;
+	}
+}
+
+my $json_str = encode_json(\@p1);
+open(my $output, '>output.json');
+print $output "$json_str";
---
+++ tests/modules.t
@@ -0,0 +1,4 @@
+#!/usr/bin/perl -w
+use Test::More tests => 2;
+use_ok( 'JSON' );
+require_ok ( 'JSON' );
